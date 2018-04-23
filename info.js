bee4b8cf0e4ccb54018c146b3ea53b5a27f113db
Alexmallick <35306214+Alexmallick@users.noreply.github.com>
2018-04-22 19:07:25 -0500

    Add files via upload

---
+++ info.js
@@ -0,0 +1,9 @@
+
+var al = new Date();
+
+
+document.write("<span id ='footer'>London assignment website</span><br />");
+document.write("<span id ='footer'>© 2018 Alex Mallick</span><br />");
+document.write("<span id ='footer'>Modified Apr 22 2018</span><br />");
+document.write(al);
+
